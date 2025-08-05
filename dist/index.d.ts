2f07087c3295ffd6310c3c2487c225047feb08aa
GitHub Action <action@github.com>
2025-08-05 15:17:47 +0000

    Release v1.0.2

---
+++ dist/index.d.ts
@@ -0,0 +1,10 @@
+import "./assets/main.css";
+import DataBsBanner from "./components/data-bs-banner.vue";
+import DataBsFooter from "./components/data-bs-footer.vue";
+import Disclaimer from "./components/disclaimer.vue";
+import DisclaimerButton from "./components/disclaimer-button.vue";
+import DisclaimerPage from "./components/disclaimer-page.vue";
+import DisclaimerView from "./components/disclaimer-view.vue";
+import SplitView from "./components/split-view.vue";
+export { DataBsBanner, DataBsFooter, DisclaimerButton, Disclaimer, DisclaimerView, DisclaimerPage, SplitView, };
+//# sourceMappingURL=index.d.ts.map
--- playground/app/pages/Disclaimer.vue
+++
@@ -1,9 +0,0 @@
-<script lang="ts" setup>
-import { DisclaimerPage } from "@dcc-bs/common-ui.bs.js";
-</script>
-
-<template>
-    <div>
-        <DisclaimerPage app-name="test-app" class="shadow-md" />
-    </div>
-</template>
