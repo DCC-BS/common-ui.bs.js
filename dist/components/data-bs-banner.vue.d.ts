a27d5bbd93075fdbc99f9e8a66b76dd3e70b98d0
GitHub Action <action@github.com>
2025-08-05 14:19:32 +0000

    Release v1.0.1

---
+++ dist/components/data-bs-banner.vue.d.ts
@@ -0,0 +1,3 @@
+declare const _default: import("vue").DefineComponent<{}, {}, {}, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, {}, string, import("vue").PublicProps, Readonly<{}> & Readonly<{}>, {}, {}, {}, {}, string, import("vue").ComponentProvideOptions, true, {}, any>;
+export default _default;
+//# sourceMappingURL=data-bs-banner.vue.d.ts.map
--- src/vue-shim.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "*.vue" {
-    import type { DefineComponent } from "vue";
-    // biome-ignore lint/suspicious/noExplicitAny: Vue component type requires any
-    const component: DefineComponent<Record<string, unknown>, Record<string, unknown>, any>;
-    export default component;
-}
