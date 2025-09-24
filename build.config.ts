b6a0557a96e6a786db75d9c3ffde28be284b6db2
Tobias Bollinger <tobias.bollinger@bs.ch>
2025-09-24 17:27:25 +0200

    Moved files so it works as a nuxt module

--- build.config.ts
+++ build.config.ts
@@ -5,10 +5,14 @@ export default defineBuildConfig({
     failOnWarn: false,
     hooks: {
         "build:done": async () => {
-            console.log("Copying main.css to dist/main.css");
-            await copyFile("src/assets/main.css", "dist/main.css");
-            console.log("Copying kantonbs directory to dist/kantonbs");
-            await cp("src/assets/kantonbs", "dist/kantonbs", { recursive: true });
+            // console.log("Copying main.css to dist/main.css");
+            // await copyFile("src/assets/main.css", "dist/main.css");
+            // console.log("Copying kantonbs directory to dist/kantonbs");
+            // await cp("src/assets/kantonbs", "dist/kantonbs", {
+            //     recursive: true,
+            // });
+            console.log("Copying lang directory to dist/lang");
+            await cp("src/lang", "dist/lang", { recursive: true });
         },
     },
 });
--- src/module.ts
+++ src/module.ts
@@ -1,31 +1,5 @@
-import { copyFile } from "node:fs/promises";
-import path from "node:path";
 import { addComponentsDir, createResolver, defineNuxtModule } from "@nuxt/kit";
 import type { ModuleRuntimeHooks } from "@nuxtjs/i18n";
-
-// import DataBsBanner from "./components/data-bs-banner.vue";
-// import DataBsFooter from "./components/data-bs-footer.vue";
-// import Disclaimer from "./components/disclaimer.vue";
-// import DisclaimerButton from "./components/disclaimer-button.vue";
-// import DisclaimerPage from "./components/disclaimer-page.vue";
-// import DisclaimerView from "./components/disclaimer-view.vue";
-// import SplitView from "./components/split-view.vue";
-// import NavigationBar from "./components/navigation-bar.vue";
-// import SplitContainer from "./components/split-container.vue";
-// import UndoRedoButtons from "./components/undo-redo-buttons.vue";
-
-// export {
-//     DataBsBanner,
-//     DataBsFooter,
-//     UndoRedoButtons,
-//     DisclaimerButton,
-//     Disclaimer,
-//     DisclaimerView,
-//     DisclaimerPage,
-//     SplitView,
-//     SplitContainer,
-//     NavigationBar,
-// };
 
 export default defineNuxtModule<ModuleRuntimeHooks>({
     meta: {
@@ -36,16 +10,6 @@ export default defineNuxtModule<ModuleRuntimeHooks>({
     defaults: {},
     setup(_options, _nuxt) {
         const resolver = createResolver(import.meta.url);
-
-        _nuxt.hook("nitro:build:public-assets", async (nitro) => {
-            const srcImage = path.resolve(__dirname, "assets/databs_logo.png");
-            const targetImage = path.join(
-                nitro.options.output.publicDir,
-                "databs_log.png",
-            );
-
-            await copyFile(srcImage, targetImage);
-        });
 
         _nuxt.hook("i18n:registerModule", (register) => {
             register({
@@ -64,7 +28,7 @@ export default defineNuxtModule<ModuleRuntimeHooks>({
         });
 
         addComponentsDir({
-            path: resolver.resolve("./components"),
+            path: resolver.resolve("./runtime/components"),
             global: true,
             pathPrefix: false,
         });
