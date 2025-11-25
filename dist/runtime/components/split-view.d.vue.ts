94ec415aa96cb8bbcffabdef13f3b44597fc9012
GitHub Action <action@github.com>
2025-11-25 10:27:26 +0000

    Release v1.1.9

---
+++ dist/runtime/components/split-view.d.vue.ts
@@ -0,0 +1,23 @@
+interface SplitViewProps {
+    aPaneStyle?: string;
+    bPaneStyle?: string;
+    splitViewStyle?: string;
+    resizerStyle?: string;
+    resizerInnerStyle?: string;
+    isHorizontal?: boolean;
+}
+declare var __VLS_1: {}, __VLS_3: {};
+type __VLS_Slots = {} & {
+    a?: (props: typeof __VLS_1) => any;
+} & {
+    b?: (props: typeof __VLS_3) => any;
+};
+declare const __VLS_base: import("vue").DefineComponent<SplitViewProps, {}, {}, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, {}, string, import("vue").PublicProps, Readonly<SplitViewProps> & Readonly<{}>, {}, {}, {}, {}, string, import("vue").ComponentProvideOptions, false, {}, any>;
+declare const __VLS_export: __VLS_WithSlots<typeof __VLS_base, __VLS_Slots>;
+declare const _default: typeof __VLS_export;
+export default _default;
+type __VLS_WithSlots<T, S> = T & {
+    new (): {
+        $slots: S;
+    };
+};
